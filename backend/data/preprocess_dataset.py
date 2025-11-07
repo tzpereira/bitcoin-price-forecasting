7b5b98b1d99364a2150f2ea284a70206e0b94df6
Mateus <99037253+mateuspdasilva@users.noreply.github.com>
2025-11-07 20:53:01 -0300

    fix: reduce batch size for dataset processing to improve memory efficiency (fuck this free vps)

--- backend/data/preprocess_dataset.py
+++ backend/data/preprocess_dataset.py
@@ -46,7 +46,7 @@ class DataPreprocessor:
         print(f"File btcusd_1-min_data.csv updated in {self.raw_path}")
 
         # Start processing the dataset
-        batch_size = 100000
+        batch_size = 50000
         processed_files = []
         i = 0
 
